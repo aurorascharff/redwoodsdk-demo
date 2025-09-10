712c4e5be2724117204e5a2ae2e6377397407713
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-10 19:59:58 +0200

    Init realtime client

--- src/client.tsx
+++ src/client.tsx
@@ -1,4 +1,8 @@
 import { initClient, initClientNavigation } from 'rwsdk/client';
+import { initRealtimeClient } from 'rwsdk/realtime/client';
 
 const { handleResponse } = initClientNavigation();
 initClient({ handleResponse });
+initRealtimeClient({
+  key: window.location.pathname,
+});
