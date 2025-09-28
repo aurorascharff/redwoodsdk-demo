c1251a4a3721ed7b817560c9eca09efae82e06b4
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-28 22:37:31 +0200

    Remove viewtrans

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable autofix/no-unused-vars */
 // @ts-expect-error - unstable API but works in React 19
-
+import { unstable_ViewTransition as ViewTransition } from 'react';
 import { link } from '@/app/shared/links';
 import type { AppContext } from '@/worker';
 import Button from '../components/ui/Button';
