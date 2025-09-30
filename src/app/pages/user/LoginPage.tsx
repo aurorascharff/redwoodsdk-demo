7160796b7baacdb16ce47999f30d47a32fb88a4d
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-30 10:43:40 +0200

    Add min w to login page

--- src/app/pages/user/LoginPage.tsx
+++ src/app/pages/user/LoginPage.tsx
@@ -57,7 +57,7 @@ export function LoginPage() {
                   className="w-full rounded-md border p-3"
                   required
                 />
-                <Button name="action" value="login" className="w-full">
+                <Button name="action" value="login" className="h-10 w-full min-w-[120px]">
                   Login
                 </Button>
               </form>
@@ -78,7 +78,7 @@ export function LoginPage() {
                   className="w-full rounded-md border p-3"
                   required
                 />
-                <Button name="action" value="register" className="w-full">
+                <Button name="action" value="register" className="h-10 w-full min-w-[140px]">
                   Register
                 </Button>
               </form>
