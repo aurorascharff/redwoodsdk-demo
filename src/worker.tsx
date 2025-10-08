4304753a835413330a5c2a4c57728b4f3f419a1b
justinvdm <justinvderm@gmail.com>
2025-10-08 02:45:31 +0200

    Add Realtime Support to Worker.

--- src/worker.tsx
+++ src/worker.tsx
@@ -1,13 +1,24 @@
-import { route, render, index, layout } from 'rwsdk/router';
+import { realtimeRoute } from 'rwsdk/realtime/worker';
+import { route, render, prefix, index, layout } from 'rwsdk/router';
 import { defineApp } from 'rwsdk/worker';
 import { type User, db } from '@/db';
 import { setCommonHeaders } from '@/app/headers';
 import { sessionMiddleware } from './session/sessionMiddleware';
 import type { Session } from './session/durableObject';
 export { SessionDurableObject } from './session/durableObject';
+import { Document } from '@/app/Document';
 import { NoJSDocument } from './app/NoJSDocument';
+import { RealtimeDocument } from './app/RealtimeDocument';
+import { apiRoutes } from './app/api/routes';
 import AppLayout from './app/layouts/AppLayout';
+import MainLayout from './app/layouts/MainLayout';
 import { HomePage } from '@/app/pages/HomePage';
+import { userRoutes } from '@/app/pages/user/routes';
+import { RealtimePage } from './app/pages/realtime/RealtimePage';
+import FancyTodosPage from './app/pages/todos/FancyTodosPage';
+import SimpleTodosPage from './app/pages/todos/SimpleTodosPage';
+export { ReactionsDurableObject } from './app/pages/realtime/reactionsDurableObject';
+export { RealtimeDurableObject } from 'rwsdk/realtime/durableObject';
 
 export type AppContext = {
   session: Session | null;
@@ -27,8 +38,12 @@ export default defineApp([
       });
     }
   },
+  realtimeRoute(env => {
+    return env.REALTIME_DURABLE_OBJECT;
+  }),
 
-  // Basic Route Handlers
+  // Route handlers
+  prefix('/api', apiRoutes),
   route('/ping', () => {
     return new Response('pong', { status: 200 });
   }),
@@ -36,11 +51,34 @@ export default defineApp([
     const { name } = requestInfo.params;
     return new Response(`Hello ${name}`, { status: 200 });
   }),
+  route('/hello', () => {
+    return <h1>Hello World!</h1>;
+  }),
 
-  // Initial SSR Page
+  // Hydrated Pages (with client-side JavaScript)
+  render(Document, [
+    layout(AppLayout, [
+      index(HomePage),
+      layout(MainLayout, [
+        prefix('/user', userRoutes),
+        route('/todos', FancyTodosPage),
+      ]),
+    ]),
+  ]),
+
+  // Real-time Pages (streaming RSC over WebSockets)
+  render(RealtimeDocument, [
+    layout(AppLayout, [
+      route('/realtime', RealtimePage),
+    ])
+  ]),
+
+  // SSR Pages (no client-side JavaScript)
   render(NoJSDocument, [
     layout(AppLayout, [
-      index(HomePage)
+      layout(MainLayout, [
+        route('/todos/simple', SimpleTodosPage)
+      ])
     ]),
   ]),
 ]);
