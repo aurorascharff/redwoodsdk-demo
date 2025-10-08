be68d5c1baff311120db2234ce3b268d60e33199
justinvdm <justinvderm@gmail.com>
2025-10-08 02:46:27 +0200

    Remove unused imports and export.

--- src/worker.tsx
+++ src/worker.tsx
@@ -1,26 +1,27 @@
 import { realtimeRoute } from 'rwsdk/realtime/worker';
 import { route, render, prefix, index, layout } from 'rwsdk/router';
 import { defineApp } from 'rwsdk/worker';
-import { type User, db } from '@/db';
-import { setCommonHeaders } from '@/app/headers';
-import { sessionMiddleware } from './session/sessionMiddleware';
-import type { Session } from './session/durableObject';
-export { SessionDurableObject } from './session/durableObject';
 import { Document } from '@/app/Document';
+import { setCommonHeaders } from '@/app/headers';
+import { HomePage } from '@/app/pages/HomePage';
+import { userRoutes } from '@/app/pages/user/routes';
+import { type User, type PrismaClient, db } from '@/db';
 import { NoJSDocument } from './app/NoJSDocument';
 import { RealtimeDocument } from './app/RealtimeDocument';
 import { apiRoutes } from './app/api/routes';
 import AppLayout from './app/layouts/AppLayout';
 import MainLayout from './app/layouts/MainLayout';
-import { HomePage } from '@/app/pages/HomePage';
-import { userRoutes } from '@/app/pages/user/routes';
 import { RealtimePage } from './app/pages/realtime/RealtimePage';
 import FancyTodosPage from './app/pages/todos/FancyTodosPage';
 import SimpleTodosPage from './app/pages/todos/SimpleTodosPage';
+import { sessionMiddleware } from './session/sessionMiddleware';
+import type { Session } from './session/durableObject';
+export { SessionDurableObject } from './session/durableObject';
 export { ReactionsDurableObject } from './app/pages/realtime/reactionsDurableObject';
 export { RealtimeDurableObject } from 'rwsdk/realtime/durableObject';
 
 export type AppContext = {
+  db: PrismaClient;
   session: Session | null;
   user: User | null;
 };
@@ -41,7 +42,6 @@ export default defineApp([
   realtimeRoute(env => {
     return env.REALTIME_DURABLE_OBJECT;
   }),
-
   // Route handlers
   prefix('/api', apiRoutes),
   route('/ping', () => {
@@ -54,31 +54,24 @@ export default defineApp([
   route('/hello', () => {
     return <h1>Hello World!</h1>;
   }),
-
-  // Hydrated Pages (with client-side JavaScript)
   render(Document, [
     layout(AppLayout, [
       index(HomePage),
       layout(MainLayout, [
         prefix('/user', userRoutes),
-        route('/todos', FancyTodosPage),
-      ]),
+        route('/todos', FancyTodosPage)]),
     ]),
   ]),
-
-  // Real-time Pages (streaming RSC over WebSockets)
   render(RealtimeDocument, [
     layout(AppLayout, [
       route('/realtime', RealtimePage),
     ])
   ]),
-
-  // SSR Pages (no client-side JavaScript)
   render(NoJSDocument, [
     layout(AppLayout, [
       layout(MainLayout, [
         route('/todos/simple', SimpleTodosPage)
       ])
-    ]),
+    ])
   ]),
 ]);
