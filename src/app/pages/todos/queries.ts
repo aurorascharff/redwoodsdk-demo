fd1ff3401c06911c63a4ca7f82b46042b2952cb3
justinvdm <justinvderm@gmail.com>
2025-10-08 04:05:11 +0200

    Revert "Revert "Avoid module level db""
    This reverts commit 151b7df134a4dc4bd375089d686690226b6f6ad9.

--- src/app/pages/todos/queries.ts
+++ src/app/pages/todos/queries.ts
@@ -1,11 +1,11 @@
 import type { Todo } from '@/db';
-import { db } from '@/db';
+import { getDb } from '@/db';
 import { slow } from '@/utils/slow';
 
 export async function getTodos(): Promise<Todo[]> {
   await slow();
 
-  const todos = await db.todo.findMany({
+  const todos = await getDb().todo.findMany({
     orderBy: { createdAt: 'desc' },
   });
   return todos;
